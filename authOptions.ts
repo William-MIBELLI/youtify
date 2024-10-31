3db63f88cced84a6096d438a1cd5baf5c1a82d05
William Mibelli <william.mibelli@gmail.com>
2024-10-31 17:56:20 +0100

    fix authOption for vercel

---
+++ authOptions.ts
@@ -0,0 +1,33 @@
+import { NextAuthOptions, AuthOptions } from "next-auth"
+
+export const authOptions : NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+      authorization: {
+        params: {
+          scope: 'openid email profile https://www.googleapis.com/auth/youtube.readonly'
+          // ou pour un accès complet :
+          // scope: 'openid email profile https://www.googleapis.com/auth/youtube'
+        }
+      }, 
+    }),
+    // ...add more providers here
+  ],
+  callbacks: {
+    jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token
+      }
+      return token
+    },
+    session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      (session as any).accessToken = token.accessToken
+      return session
+    }
+  }
+} satisfies AuthOptions
